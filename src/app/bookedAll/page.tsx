7380e38a046756e6e5aa7ea8f93900cd0a576a60
franvinkus <vincentkurnia29@gmail.com>
2025-03-06 14:31:28 +0700

    added Pagination to bookedAllTickets

--- src/app/bookedAll/page.tsx
+++ src/app/bookedAll/page.tsx
@@ -8,16 +8,30 @@ interface Ticket {
   ticketName: string;
   categoryName: string;
   eventDate: string;
+  totalPages: number;
 }
 
 const ViewAllBookTickets = () => {
   const [ticket, setTicket] = useState<Ticket[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [currentPage, setPage] = useState(1);
+  const pageSize = 10;
+  const [totalPage, setTotalPages] = useState(1);
+  const [filters, setFilter] = useState({
+        pageNumber: "1",
+        pageSize: "10",
+        totalPages:"1"
+    });
 
   useEffect(() => {
     const fetchTicket = async () => {
       try{
-          const res = await fetch("https://localhost:7059/api/v1/Test-Show-All-Data",{
+          const params = new URLSearchParams(Object.fromEntries(
+            Object.entries(filters).filter(([_, value]) => value !== "")
+        ))
+          params.set("pageNumber", currentPage.toString());
+          params.set("pageSize", pageSize.toString());
+          const res = await fetch(`https://localhost:7059/api/v1/Test-Show-All-Data?${params}`,{
             method: "GET",
             headers: { "Content-Type": "application/json" }
          });
@@ -25,17 +39,24 @@ const ViewAllBookTickets = () => {
         if(!res.ok){
           throw new Error(`Failed To Fetch: ${res.status}`);
         }
-        const data: Ticket[] = await res.json();
-        const flattenedTickets: Ticket[] = data.flatMap((category: any) => 
-          category.bookedTickets.map((ticket: any) => ({
-            id: ticket.id,
-            ticketCode: ticket.bookedTicketCode,
-            ticketName: ticket.bookedTicketName,
-            categoryName: category.bookedCategoryName,
-            eventDate: ticket.bookedEventDate
-          }))
-        );
-  
+        const data = await res.json();
+        console.log("API Response:", data);
+        setTotalPages(data.totalPages || 1)
+
+        const flattenedTickets: Ticket[] = Array.isArray(data.bookedTickets) 
+        ? data.bookedTickets.flatMap((category: any) =>
+          Array.isArray(category.bookedTickets) 
+            ? category.bookedTickets.map((ticket: any) => ({
+                id: ticket.id,
+                ticketCode: ticket.bookedTicketCode,
+                ticketName: ticket.bookedTicketName,
+                categoryName: category.bookedCategoryName,
+                eventDate: ticket.bookedEventDate
+              }))
+            : []
+        ) 
+      : [];
+
         setTicket(flattenedTickets);
       }catch(error){
         console.error("Error Fetching Data..", error);
@@ -45,7 +66,7 @@ const ViewAllBookTickets = () => {
     }
 
     fetchTicket();
-  }, []);
+  }, [currentPage]);
 
   useEffect(() => {
     console.log("Updated Ticket State:", ticket);
@@ -55,7 +76,7 @@ const ViewAllBookTickets = () => {
 
   return (
     <div className="flex justify-center items-center h-screen flex-col m-10">
-      <div className='flex items-start justify-start'>
+      <div className='-ml-96 flex items-start justify-start'>
           <Link href="/">
             <button className="rounded-2xl border-2 border-black p-2 hover:text-white hover:bg-black hover:border-white hover:cursor-pointer">
               back
@@ -63,6 +84,23 @@ const ViewAllBookTickets = () => {
           </Link>
         </div>
         <h1 className="text-3xl mb-3">Booked Tickets</h1>
+
+        <div className='mb-3'>
+          <button className='text-2xl hover:cursor-pointer' onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}> 
+            {"<"} 
+          </button>
+
+          <span className='mx-3 '>
+            Page {currentPage} ..of {totalPage}
+          </span>
+
+          <button className='text-2xl hover:cursor-pointer ' onClick={() => setPage(prev => (prev < totalPage ? prev + 1 : prev))}  
+            disabled={currentPage >= totalPage}> 
+            {">"} 
+          </button>
+        </div>
+
         {loading ? (
           <p>loading..</p>
         ) : ticket.length > 0 ?(
